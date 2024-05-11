914aaefaf105be1b0731a3782b57c98608a52043
Satyam Shivam <95932086+isatyamshivam@users.noreply.github.com>
2024-05-11 10:11:37 +0000

    learn datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";           // this is used to treat all JS code as newer version in the file. it works automatically, so you don't have to write this again in any file.
+
+// alert( 3 + 3)        // we are using nodejs, not browser
+
+/*
+    there are two types of datatypes
+        1. primitive datatypes
+        2. non-primitive datatypes 
+*/
+
+/* 
+    1. primitive datatypes
+
+        i.    number => 0 - 2 to the power 53
+        ii.   bigint => infinite
+        iii.  string => ""
+        iv.   boolean => true/false
+        v.    null => standalone value
+        vi.   undefined => 
+        vii.  symbol => this is used in unique value type
+*/
+
+/*
+    2. non-primitive datatypes
+        i.    object
+        ii.   array
+*/
+
+console.log(typeof undefined);    // undefined
+console.log(typeof null);         // object
